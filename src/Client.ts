d36e46b6714f7a6be5c04cf2a6d3e1d16544511a
ryan <rhawkins030@gmail.com>
2025-02-05 20:53:17 -0500

    [feature] posts v1

--- src/Client.ts
+++ src/Client.ts
@@ -14,6 +14,7 @@ import {
   RawChannel,
   RawMessage,
   RawMessageButton,
+  RawPost,
   RawServer,
   RawServerMember,
   RawUser,
@@ -25,6 +26,7 @@ import {
   postMessage,
 } from "./services/MessageService";
 import { path, updatePath } from "./services/serviceEndpoints";
+import { deletePost, editPost, getPosts, postPost } from "./services/PostsService";
 
 export const Events = ClientEvents;
 
@@ -35,6 +37,7 @@ export class Client extends EventEmitter<ClientEventMap> {
   users: Users;
   channels: Channels;
   servers: Servers;
+  posts: Posts;
 
   constructor(opts?: { urlOverride?: string }) {
     super();
@@ -48,6 +51,7 @@ export class Client extends EventEmitter<ClientEventMap> {
     this.channels = new Channels(this);
     this.users = new Users(this);
     this.servers = new Servers(this);
+    this.posts = new Posts(this);
     new EventHandlers(this);
   }
 
@@ -335,6 +339,13 @@ export interface MessageOpts {
   silent?: boolean;
 }
 
+export interface PostOpts {
+  nerimityCdnFileId?: string;
+  poll?: {
+    choices: string[];
+  };
+}
+
 export class Channel {
   client: Client;
   id: string;
@@ -463,6 +474,93 @@ export class Message {
   toString() {
     return `[q:${this.id}]`;
   }
+}
+
+class Posts {
+  client: Client;
+  constructor(client: Client) {
+    this.client = client;
+  }
+
+  async get(id?: string) {
+    const RawPosts = await getPosts(this.client);
+    const posts = RawPosts.map((post) => new Post(this.client, post));
+    return id ? posts.find((p) => p.id === id) : posts;
+  }
+
+  async create(content: string, opts?: PostOpts) {
+    const RawPost = await postPost({
+      client: this.client,
+      content: content,
+      nerimityCdnFileId: opts?.nerimityCdnFileId,
+      poll: opts?.poll,
+    });
+
+    const post = new Post(this.client, RawPost);
+
+    return post;
+  }
+}
+
+export class Post {
+  client: Client;
+  id: string;
+  content?: string;
+  attachments?: Array<any>;
+  deleted: boolean;
+  block?: boolean;
+  commentToId: string;
+  commentTo?: RawPost;
+  createdBy: RawUser;
+  createdAt: number;
+  editedAt: number;
+  likedBy: { id: string }[]; // if you liked this post, array will not be empty
+  reposts: { id: string; createdBy: { id: string; username: string } }[];
+  repost?: RawPost;
+  _count: { likedBy: number; comments: number; reposts: number };
+  views: number;
+  announcement: any;
+  poll?: any;
+
+  constructor(client: Client, post: RawPost) {
+    this.client = client;
+    this.id = post.id;
+    this.content = post.content;
+    this.attachments = post.attachments;
+    this.deleted = post.deleted;
+    this.block = post.block;
+    this.commentToId = post.commentToId;
+    this.commentTo = post.commentTo;
+    this.createdBy = post.createdBy;
+    this.createdAt = post.createdAt;
+    this.editedAt = post.editedAt;
+    this.likedBy = post.likedBy;
+    this.reposts = post.reposts;
+    this.repost = post.repost;
+    this._count = post._count;
+    this.views = post.views;
+    this.announcement = post.announcement;
+    this.poll = post.poll;
+  }
+
+  async edit(content: string) {
+    const RawPost = await editPost({
+      client: this.client,
+      content: content,
+      postId: this.id,
+    })
+
+    const post = new Post(this.client, RawPost);
+    return post;
+  }
+
+  async delete() {
+    await deletePost({
+      client: this.client,
+      postId: this.id,
+    })
+  }
+
 }
 
 class User {
--- src/services/serviceEndpoints.ts
+++ src/services/serviceEndpoints.ts
@@ -10,6 +10,12 @@ const GetMessages = (channelId: string) => `${BaseUrl}/channels/${channelId}/mes
 const PostMessage = (channelId: string) => `${BaseUrl}/channels/${channelId}/messages`;
 const EditMessage = (channelId: string, messageId: string) => `${BaseUrl}/channels/${channelId}/messages/${messageId}`;
 
+// Posts
+const GetPosts = () => `${BaseUrl}/posts`;
+const PostPost = () => `${BaseUrl}/posts`;
+const EditPost = (postId: string) => `${BaseUrl}/posts/${postId}`;
+const DeletePost = (postId: string) => `${BaseUrl}/posts/${postId}`;
+
 
 const ButtonClickCallback = (channelId: string, messageId: string, buttonId: string) => `${BaseUrl}/channels/${channelId}/messages/${messageId}/buttons/${buttonId}/callback`;
 
@@ -17,5 +23,9 @@ export const ServiceEndpoints = {
     GetMessages,
     PostMessage,
     EditMessage,
+    GetPosts,
+    PostPost,
+    EditPost,
+    DeletePost,
     ButtonClickCallback
 };
