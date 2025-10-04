aa98cf4c98a433c2367f4d43a6cd64cf34f3d5b4
Asraye <asrayeofficial@gmail.com>
2025-10-04 19:00:21 +1000

    feat: add webhooks

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { Client, Events } from "./Client";
 export { AttachmentBuilder } from "./Attachment";
 export { RPCClient, EmitPayload } from "./RPCClient";
+export { WebhookBuilder } from "./Webhooks"; 
