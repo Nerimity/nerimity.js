2de8ab16dbf1fd7e05b18cc8b85dc4a9414719d4
Supertiger <saawalkhn@googlemail.com>
2025-12-08 10:53:01 +0000

    export classes

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,17 @@ export { Client, Events } from "./classes/Client";
 export { AttachmentBuilder } from "./classes/Attachment";
 export { RPCClient, EmitPayload } from "./classes/RPCClient";
 export { WebhookBuilder } from "./classes/Webhooks";
+
+export { Channel } from "./classes/Channel";
+export { ServerChannel } from "./classes/ServerChannel";
+export { Message } from "./classes/Message";
+export { MessageButton } from "./classes/MessageButton";
+export { Post } from "./classes/Post";
+export { ServerMember } from "./classes/ServerMember";
+export { ServerRole } from "./classes/ServerRole";
+export { Server } from "./classes/Server";
+export { User } from "./classes/User";
+export { Collection } from "./classes/Collection";
+export { ClientUser } from "./classes/ClientUser";
+
 export { RolePermissions } from "./bitwise";
